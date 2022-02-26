380ba1f82df44c75b5996d6587d04eee4b30deda
David Turner <95373983+DeToxFox@users.noreply.github.com>
2022-02-26 11:04:22 -0330

    Add files via upload

---
+++ DTfunction.py
@@ -0,0 +1,23 @@
+personName = input('Enter your name: ')
+tripLocal = input('Enter the trip location: ')
+tripDays = int(input('Enter the number of days: '))
+numKms = int(input('Enter the number of Kms traveled: '))
+print()
+perDiem = 56.00
+lodging = 125.00
+totalDiem = tripDays * perDiem
+totalLodging = (tripDays - 1) * lodging
+totalBasedDays = totalDiem + totalLodging
+
+costPerKms = 0.24
+totalKms = numKms * costPerKms
+
+totalClaim = totalKms + totalBasedDays
+
+print("Question 5: NL Chocolate Company Employee Expenses")
+print('Name:                   ', personName)
+print('Location Traveled:      ', tripLocal)
+print('Length of Stay (days):  ', tripDays)
+print('Number of Kms traveled: ', numKms)
+print()
+print('Total claim amount:      $', totalClaim)
