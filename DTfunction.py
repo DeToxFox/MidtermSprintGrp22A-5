075461bc99a30b6b848505edbc5db5dadb5fbb30
David Turner <95373983+DeToxFox@users.noreply.github.com>
2022-03-03 16:11:17 -0330

    Add files via upload

--- DTfunction.py
+++ DTfunction.py
@@ -10,10 +10,16 @@ BONUS_DAYS_AMOUNT = 100.00
 DEC_BONUS_RATE = 50.00
 HST = 0.15
 
+
 while True:
     while True:
+        # Welcome message
+        print()
+        print("       ------------------------------------")
+        print("         Option 1: Employee Travel Claim")
+        print("       ------------------------------------")
         # input and validation of the employee number
-        Emp_Num = input("Please enter in your 5 digit employee numer (12345):  ")
+        Emp_Num = input("Please enter in your 5 digit employee numer (12345): ")
         if Emp_Num == "":
             print("Your employee number cannot be blank - please re-enter.")
         elif len(Emp_Num) != 5:
@@ -26,7 +32,7 @@ while True:
     # Input and validation of the employee's first name
     while True:
         Allowed_Char = set("ABCDEFGHIJKLMONPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz1234567890-'")
-        FirstName = input("Enter the your first name (type End to exit): ").title()
+        FirstName = input("Enter the your first name (type End to exit):        ").title()
         if FirstName == "End":
             exit()
         elif FirstName == "":
@@ -39,7 +45,7 @@ while True:
     # Input and validation of the employee's last name
     while True:
         Allowed_Char = set("ABCDEFGHIJKLMONPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz1234567890-'")
-        LastName = input("Enter the your last name: ").title()
+        LastName = input("Enter the your last name:                            ").title()
         if LastName == "":
             print("Last name must not be blank - please re-enter.")
         elif set(LastName).issubset(Allowed_Char) == False:
@@ -50,7 +56,7 @@ while True:
     # Input and validation for the employee trip location
     while True:
         Allowed_Char = set("ABCDEFGHIJKLMONPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz.-'")
-        Location = input("Enter the trip location: ").title()
+        Location = input("Enter the trip location:                             ").title()
         if Location == "":
             print("Trip location can't be blank - please re-enter")
         elif set(Location).issubset(Allowed_Char) == False:
@@ -61,7 +67,7 @@ while True:
     # Input and validation for the employee trip distance
     while True:
         try:
-            StartDate = input("Enter the start date of the claim (YYYY-MM-DD): ")
+            StartDate = input("Enter the start date of the claim (YYYY-MM-DD):      ")
             StartDate = datetime.datetime.strptime(StartDate, "%Y-%m-%d")
         except:
             print("Start date not a valid format - please re-enter.")
@@ -69,7 +75,7 @@ while True:
             break
     while True:
         try:
-            EndDate = input("Enter the end date of the claim (YYYY-MM-DD): ")
+            EndDate = input("Enter the end date of the claim (YYYY-MM-DD):        ")
             EndDate = datetime.datetime.strptime(EndDate, "%Y-%m-%d")
         except:
             print("End date not a valid format - please re-enter.")
@@ -88,7 +94,7 @@ while True:
     # Input and validation of vehicle used
     while True:
         Allowed_Char = set("RO")
-        Vehicle_Type = input("Enter the vehicle used, own or rental (O or R): ").upper()
+        Vehicle_Type = input("Enter the vehicle used, own or rental (O or R):      ").upper()
         if Vehicle_Type == "":
             print("Vehicle type can't be blank - please re-enter")
         elif set(Vehicle_Type).issubset(Allowed_Char) == False:
@@ -103,12 +109,14 @@ while True:
     Vehicle_Payout_Bonus = 0
 
     if Vehicle_Type == "R":
+        Vehicle_Type_Selected = "Rental"
         Vehicle_Payout = NumDays * RENTAL_CAR_RATE
-        Msg = "The Rental Payout is "
+        Msg = "Rental Amount:"
     elif Vehicle_Type == "O":
+        Vehicle_Type_Selected = "Personal"
         while True:
             try:
-                Kms_Travel = float(input("How many kms where traveled on the work trip?: "))
+                Kms_Travel = float(input("How many kms were traveled on the work trip?:        "))
             except:
                 print("Trade in value is not a valid number - please re-enter.")
             if Kms_Travel > 2000:
@@ -117,19 +125,19 @@ while True:
                 print("Kms travelled for work must be greater than 0 kms - please re-enter")
             elif Kms_Travel > 1000:
                 Vehicle_Payout = Kms_Travel * (PER_KM_RATE)
-                Msg = "Your travel pay "
+                Msg = "Mileage Amount:"
                 KMP = f"The kms traveled {Kms_Travel:.2f}"
                 break
             else:  # int(Kms_T) < 1000:
                 Vehicle_Payout = Kms_Travel * PER_KM_RATE
-                Msg = "Your travel pay "
+                Msg = "Mileage Amount:"
                 KMP = f"The kms traveled {Kms_Travel:.2f}"
                 break
 
     # Input and validation of claim type
     while True:
         Allowed_Char = set("SE")
-        Claim_Type = input("Enter claim type Standard or Executive (S or E): ").upper()
+        Claim_Type = input("Enter claim type Standard or Executive (S or E):     ").upper()
         if Claim_Type == "":
             print("Claim type can't be blank - please re-enter")
         elif set(Claim_Type).issubset(Allowed_Char) == False:
@@ -156,12 +164,12 @@ while True:
     # Calculate bonus rate for executive claims
     if Claim_Type == "S":
         Claim_Type_Bonus = 0
-        MemberTypeDsp = "STANDARD"
+        ClaimTypeDsp = "STANDARD"
     else:
         Claim_Type_Bonus = NumDays * EXECUTIVE_RATE
         ClaimTypeDsp = "EXECUTIVE"
 
-    # Calculate start and end date bonus
+    # Calculate start and end date bonus for December
     SDDate = StartDate
     Start_Date_Year = SDDate.year
     Start_Date_Month = 12
@@ -180,7 +188,7 @@ while True:
         Special_Dates_Bonus = 0
 
     # Calculating the bonus
-    Bonus = Days_Amt_Bonus + Kms_Bonus + Claim_Type_Bonus + Special_Dates_Bonus
+    Bonus_Total = Days_Amt_Bonus + Kms_Bonus + Claim_Type_Bonus + Special_Dates_Bonus
 
     # Calculating per diem amount
     Per_Diem = NumDays * DAILY_RATE
@@ -189,29 +197,73 @@ while True:
     Tax = Per_Diem * HST
 
     # Calculating claim amount
-    Claim_Amount = Per_Diem + Vehicle_Payout + Bonus
+    Claim_Amount = Per_Diem + Vehicle_Payout + Bonus_Total
 
     # Calculate total claim including tax
     Total_Claim = Claim_Amount + Tax
 
     # Formatting for final display
-    VPDsp = "${:,.2f}".format(Vehicle_Payout)
+    TaxDsp = "${:,.2f}".format(Tax)
+    Claim_AmountDsp = "${:,.2f}".format(Claim_Amount)
+    Total_ClaimDsp = "${:,.2f}".format(Total_Claim)
+    Vehicle_PayoutDsp = "${:,.2f}".format(Vehicle_Payout)
     Per_DiemDsp = "${:,.2f}".format(Per_Diem)
-
-    print()
+    Days_Amt_BonusDsp = "${:,.2f}".format(Days_Amt_Bonus)
+    Kms_BonusDsp = "${:,.2f}".format(Kms_Bonus)
+    Claim_Type_BonusDsp = "${:,.2f}".format(Claim_Type_Bonus)
+    Special_Dates_BonusDsp = "${:,.2f}".format(Special_Dates_Bonus)
+    Bonus_TotalDsp = "${:,.2f}".format(Bonus_Total)
+    StartDateDsp = StartDate.strftime("%d-%b-%y")
+    EndDateDsp = EndDate.strftime("%d-%b-%y")
 
     # Display of all inputs and calculated values
-
-    print("3 days or more bonus", Days_Amt_Bonus)
-    print("1000 plus kms bonus", Kms_Bonus)
-    print("Executive Claim Bonus", Claim_Type_Bonus)
-    print("December bonus", Special_Dates_Bonus)
-    print("This is total bonus", Bonus)
-
-    # Testing purposes origianally possibly delete later
-    print("Per Diem: ", Per_DiemDsp)
-    print(Msg, VPDsp)
-    print(KMP)
+    print()
+    print()
+    print()
+    print("-" * 38)
+    print(" " * 7, "YOUR CLAIMS STATEMENT")
+    print("-" * 38)
+    print("{:<25} {:<5}".format("Employee Number:", Emp_Num))
+    print("{:<25} {:<12}".format("Employee First Name:", FirstName))
+    print("{:<25} {:<12}".format("Employee Last Name:", LastName))
+    print("{:<25} {:<12}".format("Location Traveled:", Location))
+    print("{:<25} {:<9}".format("Travel Start Date:", StartDateDsp))
+    print("{:<25} {:<9}".format("Travel End Date:", EndDateDsp))
+    print("{:<25} {:<16}".format("Vehicle Type Used:", Vehicle_Type_Selected))
+    print("{:<25} {:<12}".format("Claim Type Selected:", ClaimTypeDsp))
+    print("{:<25} {:<3}".format("Trip Duration:", str(NumDays)+" days"))
+    print()
+    print("-" * 38)
+    print(" " * 10, "Bonus Breakdown")
+    print("-" * 38)
+    print("{:<28} {:>9}".format("Away 3 Days or More:", Days_Amt_BonusDsp))
+    print()
+    print("{:<54} {:>6}".format("Traveled over 1000 kms\n(Personal Vehicle Only):", Kms_BonusDsp))
+    print()
+    print("{:<45} {:>9}".format("Claim Type Bonus\n(Executive Claim Only):", Claim_Type_BonusDsp))
+    print()
+    print("{:<43} {:>9}".format("December Bonus\n(Between Dec 15-22):", Special_Dates_BonusDsp))
+    print(" " * 26, "-" * 11)
+    print("{:<28} {:>9}".format("Total Bonus:", Bonus_TotalDsp))
+    # maybe take out "Total Bonus" as its displayed lower down in the subtotal
+    print()
+    print("-" * 38)
+    print(" " * 12, "Claim Amount")
+    print("-" * 38)
+    print("{:<30} {:>7}".format("Per Diem Amount:", Per_DiemDsp))
+    print("{:<30} {:>7}".format("Total Bonus:", Bonus_TotalDsp))
+    print("{:<30} {:>7}".format(Msg, Vehicle_PayoutDsp))
+    print(" " * 26, "-" * 11)
+    print("{:<28} {:>9}".format("Claim Amount:", Claim_AmountDsp))
+    print()
+    print("-" * 38)
+    print(" " * 12, "Total Claim")
+    print("-" * 38)
+    print("{:<28} {:>7}".format("Claim Amount:", Claim_AmountDsp))
+    print("{:<30} {:>7}".format("HST Amount:", TaxDsp))
+    print(" " * 26, "-" * 11)
+    print("{:<28} {:>9}".format("Claim Total:", Total_ClaimDsp))
+    print()
 
     while True:
         Continue = input("Do you want to process another claim (Y / N): ").upper()
